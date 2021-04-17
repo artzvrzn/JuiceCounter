4b1247fa900a71268cc00f36ec4fb7c5c98f59a0
artem <jojoba37544@yandex.ru>
2021-04-17 05:23:57 +0300

    new class to handle lx02 export

--- chrome_driver.py
+++ chrome_driver.py
@@ -1,26 +1,25 @@
 import pyautogui as gui
 from selenium import webdriver
 from datetime import date, timedelta
+from selenium.webdriver.common.keys import Keys
+from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.common.exceptions import NoSuchFrameException
 
-from time import sleep
-
-driver = webdriver.Chrome('C:\\Users\\by059491\\PycharmProjects\\JuiceCounter\\chromedriver.exe')
-driver.implicitly_wait(10)
-
 
 class GetPage:
     def __init__(self, page_url):
-        driver.get(page_url)
+        self.driver = webdriver.Chrome('C:\\Users\\by059491\\PycharmProjects\\JuiceCounter\\chromedriver.exe')
+        self.driver.implicitly_wait(10)
+        self.driver.get(page_url)
+        self.driver.switch_to.frame('application-Shell-startGUI')
         self.fill_parameters = {}
-        self.submit = None
 
     def fill_start_page(self):
         for id_key, id_val in self.fill_parameters.items():
-            element = driver.find_element_by_id(id_key)
+            element = self.driver.find_element_by_id(id_key)
             element.clear()
             element.send_keys(id_val)
 
@@ -39,7 +38,7 @@ class OutOfStock(GetPage):
 
     def __init__(self, page_url, date_from=None, date_to=None):
         super().__init__(page_url)
-        driver.switch_to.frame('application-Shell-startGUI')
+
         self.date_from = date.today() + timedelta(days=1)
         self.date_to = date_to
         self.fill_parameters = {
@@ -48,8 +47,8 @@ class OutOfStock(GetPage):
             self.DOC_TYPE: 'ZOR',
         }
         self.checkboxes = [
-            driver.find_element_by_id(self.CHECKBOX_ONE),
-            driver.find_element_by_id(self.CHECKBOX_TWO),
+            self.driver.find_element_by_id(self.CHECKBOX_ONE),
+            self.driver.find_element_by_id(self.CHECKBOX_TWO),
         ]
         if date_from is not None:
             self.fill_parameters[self.DEL_DATE_FROM] = date_from
@@ -60,49 +59,48 @@ class OutOfStock(GetPage):
         super().fill_start_page()
         for checkbox in self.checkboxes:
             checkbox.click()
-        submit = driver.find_element_by_id(self.SUBMIT_BTN)
-        submit.click()
+        self.driver.find_element_by_id(self.SUBMIT_BTN).click()
 
     def export_file(self):
-        driver.find_element_by_id(self.EXPORT_BTN).click()
-        driver.find_element_by_id(self.SPREADSHEET).find_element_by_tag_name('tr').click()
-        driver.find_element_by_id(self.CONTINUE_BTN).click()
+        self.driver.find_element_by_id(self.EXPORT_BTN).click()
+        self.driver.find_element_by_id(self.SPREADSHEET).find_element_by_tag_name('tr').click()
+        self.driver.find_element_by_id(self.CONTINUE_BTN).click()
 
 
-# SALES_ORG_ID = 'M0:46:::1:34'
-# DEL_DATE_FROM_ID = 'M0:46:::3:34'
-# DEL_DATE_TO_ID = 'M0:46:::3:59'
-# DOC_TYPE_ID = 'M0:46:::6:34'
-# CHECKBOX_ONE_ID = 'M0:46:::16:3'
-# CHECKBOX_TWO_ID = 'M0:46:::17:3'
-# 'M0:50::btn[8]-cnt'
-# 'thead'
-# 'menu_MB_EXPORT102_1_1-r' # cписок
-# 'u1BC56D'
-# EXPORT_BTN_ID = '_MB_EXPORT102-r'
-#
-#
-#
-#
-# driver.get('https://cuvl0301.eur.cchbc.com:8204/sap/bc/ui2/flp#Shell-startGUI?sap-ui2-tcode=ZSD_OOS&sap-system=LOCAL')
-# driver.switch_to.frame(FRAME)
-#
-#
-#
-# checkbox_1 = driver.find_element_by_id(CHECKBOX_ONE_ID)
-# checkbox_1.click()
-# checkbox_2 = driver.find_element_by_id(CHECKBOX_TWO_ID)
-# checkbox_2.click()
-# submit = driver.find_element_by_id('M0:50::btn[8]')
-# submit.click()
-# export = driver.find_element_by_id(EXPORT_BTN_ID)
-# export.click()
-# spreadsheet = driver.find_element_by_id('menu_MB_EXPORT102_1_1-r').find_element_by_tag_name('tr')
-# spreadsheet.click()
-# continue_btn = driver.find_element_by_id('M1:50::btn[0]')
-# continue_btn.click()
+class Lx02(GetPage):
+    WAREHOUSE_NUMBER = 'M0:46:::0:34'
+    STORAGE_TYPE_FROM = 'M0:46:::1:34'
+    STORAGE_TYPE_TO = 'M0:46:::1:59'
+    PLANT = 'M0:46:::6:34'
+    LAYOUT = 'M0:46:::18:34'
+    SUBMIT_BTN = 'M0:50::btn[8]'
+    CONTINUE_BTN = 'M1:50::btn[0]'
+
+    def __init__(self, page_url):
+        super().__init__(page_url)
+        self.fill_parameters = {
+            self.WAREHOUSE_NUMBER: '270',
+            self.STORAGE_TYPE_FROM: '110',
+            self.STORAGE_TYPE_TO: '200',
+            self.PLANT: '2310',
+            self.LAYOUT: 'SLED/BBD',
+        }
+
+    def fill_start_page(self):
+        super().fill_start_page()
+        self.driver.find_element_by_id(self.SUBMIT_BTN).click()
+
+    def export_file(self):
+        body = self.driver.find_element_by_tag_name('body')
+        sleep(0.7)
+        body.send_keys(Keys.SHIFT + Keys.F4)
+        self.driver.find_element_by_id(self.CONTINUE_BTN).click()
 
 
-zsd_oos = OutOfStock('https://cuvl0301.eur.cchbc.com:8204/sap/bc/ui2/flp#Shell-startGUI?sap-ui2-tcode=ZSD_OOS&sap-system=LOCAL')
-zsd_oos.fill_start_page()
-zsd_oos.export_file()
+if __name__ == '__main__':
+    zsd_oos = OutOfStock('https://cuvl0301.eur.cchbc.com:8204/sap/bc/ui2/flp#Shell-startGUI?sap-ui2-tcode=ZSD_OOS&sap-system=LOCAL')
+    zsd_oos.fill_start_page()
+    zsd_oos.export_file()
+    lx02 = Lx02('https://cuvl0301.eur.cchbc.com:8204/sap/bc/ui2/flp#Shell-startGUI?sap-ui2-tcode=LX02&sap-system=LOCAL')
+    lx02.fill_start_page()
+    lx02.export_file()
